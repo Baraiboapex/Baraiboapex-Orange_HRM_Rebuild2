2377418774e75cc603d0b9287de6de220d1e1fc1
Matthew Bailey <MBailey@jtafla.com>
2021-07-05 12:02:39 -0400

    test

---
+++ Object Repository/LOGIN/test_obj.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test_obj</name>
+   <tag></tag>
+   <elementGuidId>6b8b6835-0f06-4098-851e-938df4aea6a7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
